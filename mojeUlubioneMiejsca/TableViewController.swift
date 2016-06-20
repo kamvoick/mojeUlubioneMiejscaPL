b54413f2570b7110cc596c491131b5dc801fa75f
Kamil Wójcik <van_kam@wp.pl>
2016-06-20 04:37:18 +0200

    reload

---
+++ mojeUlubioneMiejsca/TableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  TableViewController.swift
+//  mojeUlubioneMiejsca
+//
+//  Created by Kamil Wójcik on 19.06.2016.
+//  Copyright © 2016 Kamil Wójcik. All rights reserved.
+//
+
+import UIKit
+
+var miejsca = [Dictionary<String,String>()] //array który zawiera dict
+
+var aktywneMiejsce = -1 //umieszczamy zmienną która będzie zapisywała któro miejsce wybraliśmy z arraya, -1 oznacza że żadne nie jest wybrane
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if miejsca.count == 1{
+            miejsca.removeAtIndex(0)
+
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return miejsca.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) 
+        cell.textLabel?.text = miejsca[indexPath.row]["nazwa"]
+        
+        return cell
+    }
+    
+    override func viewWillAppear(animated: Bool) { //przed wczytaniem view
+        tableView.reloadData()
+    }
+
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        aktywneMiejsce = indexPath.row // tutaj przypisujemy
+        return indexPath
+    }
+    
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- mojeUlubioneMiejsca/ViewController.swift
+++ mojeUlubioneMiejsca/ViewController.swift
@@ -7,19 +7,122 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager: CLLocationManager!
+    @IBOutlet weak var mapa: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if aktywneMiejsce == -1 { //jeżeli nie wybrał żadnego miejsca to autoryzacja i aktualziaowanie
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+        }else{ //jeżeli wybrał miejsce to współrzedne tego miejsca i pokaż na mapie
+            let szerokośćGeogr = NSString(string: miejsca[aktywneMiejsce]["szerokośćGeogr"]!).doubleValue
+            let długośćGeogr = NSString(string: miejsca[aktywneMiejsce]["długośćGeogr"]!).doubleValue
+            //nsstring i doublevalue żeby zamienić stringi na double bo taki jest typ stopni
 
+            let szerokośćDelta: CLLocationDegrees = 0.01
+            let długośćDelta: CLLocationDegrees = 0.01
+            let coordinate = CLLocationCoordinate2D(latitude: szerokośćGeogr, longitude: długośćGeogr)
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(szerokośćDelta, długośćDelta)
+            let region: MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+            mapa.setRegion(region, animated: true)
+            
+            //pinezka
+            let pinezka = MKPointAnnotation()
+            pinezka.coordinate = coordinate
+            pinezka.title = miejsca[aktywneMiejsce]["nazwa"]
+            self.mapa.addAnnotation(pinezka)
+        }
+        
+        
+        
+        
+        let długieNaciśnięcie = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.akcja(_:)))
+        długieNaciśnięcie.minimumPressDuration = 1.0
+        mapa.addGestureRecognizer(długieNaciśnięcie)
+        
+        
+    }
+    
+    func akcja(gestureRecognizer: UIGestureRecognizer){
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began{ //kiedy długo się naciśnie zostanie tylko raz wywołane
+            
+            let dotknięcie = gestureRecognizer.locationInView(self.mapa)
+            let noweWspółrzędne = self.mapa.convertPoint(dotknięcie, toCoordinateFromView: self.mapa)
+            
+            let lokalizacja = CLLocation(latitude: noweWspółrzędne.latitude, longitude: noweWspółrzędne.longitude)
+            CLGeocoder().reverseGeocodeLocation(lokalizacja, completionHandler: { (placemarks, error: NSError?) in
+                
+                var title = ""
+                
+                if error != nil{
+                    print(error)
+                }else{
+                    if let p = placemarks?[0]{
+                        var miasto = ""
+                        var ulica = ""
+                        
+                        if p.locality != nil{
+                            miasto = p.locality!
+                        }
+                        if p.thoroughfare != nil{
+                            ulica = p.thoroughfare!
+                        }
+                        title = "\(ulica), \(miasto)"
+                    }
+                }
+                
+                if title == ""{
+                    title = "Dodano \(NSDate())"
+                }
+                
+                miejsca.append(["nazwa":title, "szerokośćGeogr": "\(noweWspółrzędne.latitude)", "długośćGeogr": "\(noweWspółrzędne.longitude)"])
+                
+                let pinezka = MKPointAnnotation()
+                pinezka.coordinate = noweWspółrzędne
+                pinezka.title = title
+                self.mapa.addAnnotation(pinezka)
+            })
+        }
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let pozycjaUżytkownika: CLLocation = locations[0]
+        let szerokośćGeogr = pozycjaUżytkownika.coordinate.latitude
+        let długośćGeogr = pozycjaUżytkownika.coordinate.longitude
+        let szerokośćDelta: CLLocationDegrees = 0.01
+        let długośćDelta: CLLocationDegrees = 0.01
+        let coordinate = CLLocationCoordinate2D(latitude: szerokośćGeogr, longitude: długośćGeogr)
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(szerokośćDelta, długośćDelta)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        mapa.setRegion(region, animated: true)
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "noweMiejsce"{
+            aktywneMiejsce = -1
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
